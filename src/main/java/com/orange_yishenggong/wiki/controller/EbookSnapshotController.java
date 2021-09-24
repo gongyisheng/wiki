d91b211c00e28cf6d7f1c957f6838ada46e9a3ed
gongyisheng <Orangepass!2019>
2021-09-24 14:30:13 -0500

    11.6 update snapshot controller

--- src/main/java/com/orange_yishenggong/wiki/controller/EbookSnapshotController.java
+++ src/main/java/com/orange_yishenggong/wiki/controller/EbookSnapshotController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ebook-snapshot")
 public class EbookSnapshotController {
 
     @Resource
--- src/main/java/com/orange_yishenggong/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/orange_yishenggong/wiki/mapper/EbookSnapshotMapperCust.java
@@ -5,6 +5,7 @@ import com.orange_yishenggong.wiki.resp.StatisticResp;
 import java.util.List;
 
 public interface EbookSnapshotMapperCust {
+
     public void genSnapshot();
 
     List<StatisticResp> getStatistic();
