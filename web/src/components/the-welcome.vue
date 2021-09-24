5cc93f6a36503cf0f57f960f6f14f19971724745
gongyisheng <Orangepass!2019>
2021-09-24 16:24:23 -0500

    11.7 import echarts and example of charts

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -100,6 +100,8 @@
 import {defineComponent, ref, onMounted} from 'vue'
 import axios from 'axios';
 
+declare let echarts: any;
+
 export default defineComponent({
   name: 'the-welcome',
   setup() {
@@ -132,11 +134,42 @@ export default defineComponent({
         }
       })
     }
+    const testEcharts = () => {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById('main'));
+
+      // 指定图表的配置项和数据
+      const option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+
     onMounted(() => {
       getStatistic();
-      //testEcharts();
+      testEcharts();
       //get30DayStatistic();
     });
+
     return {
       statistic
     }
